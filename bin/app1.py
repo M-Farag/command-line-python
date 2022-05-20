90dce0c537e5a842b9954856773d44bee5311418
M-Farag <mina.farag@icloud.com>
2022-05-20 02:23:52 +0200

    UPD App1 to accept arguments

--- bin/app1.py
+++ bin/app1.py
@@ -1,3 +1,11 @@
 #! /usr/bin/env python3
-print('hello engineers')
+# elegant greeting app
 
+import argparse
+
+parser = argparse.ArgumentParser(description='A greeting application')
+
+parser.add_argument('name',help='It is the name you wanna greet')
+args = parser.parse_args()
+
+print('Hello, {}'.format(args.name))
